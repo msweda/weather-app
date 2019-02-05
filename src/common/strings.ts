743d7b040af76371230f43f5c3d67441c179b7ed
Matthew Sweda <sweda.matthew@gmail.com>
2019-02-05 00:32:11 -0500

    MS | Shortened app heading to better fit on small screens

--- src/common/strings.ts
+++ src/common/strings.ts
@@ -1,8 +1,10 @@
-export const appHeading = 'Matthew Sweda\'s Weather App';
-export const phoneNumber = '(248)-770-7487';
-export const email = 'msweda.developer@gmail.com';
-export const location = 'Detroit, MI';
-export const copyright = (timestamp: number) => `© Matthew Sweda ${new Date(timestamp).getFullYear()}`;
-export const emailLink = `mailto:${email}`;
-export const linkedInLink = 'https://www.linkedin.com/in/matthew-sweda-118570ba/';
-export const instagramLink = 'https://www.instagram.com/mattsweda/';
+export const appHeading = "Matt's Weather App"
+export const phoneNumber = '(248)-770-7487'
+export const email = 'msweda.developer@gmail.com'
+export const location = 'Detroit, MI'
+export const copyright = (timestamp: number) =>
+  `© Matthew Sweda ${new Date(timestamp).getFullYear()}`
+export const emailLink = `mailto:${email}`
+export const linkedInLink =
+  'https://www.linkedin.com/in/matthew-sweda-118570ba/'
+export const instagramLink = 'https://www.instagram.com/mattsweda/'
